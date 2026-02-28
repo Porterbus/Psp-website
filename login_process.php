c996c150a94ea66a24f1da2404ca0281ddcb01b8
Ollie Porter <ollie.rugby.p@gmail.com>
2026-02-28 16:29:38 +0000

    Created the login php script

---
+++ login_process.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+require 'db_connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    $stmt = $conn->prepare("SELECT user_id, full_name, password_hash, role FROM Users WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user['password_hash'])) {
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['role'] = $user['role'];
+            $_SESSION['name'] = $user['full_name'];
+
+            echo "<h1>Login succesful! Welcome, " . $_SESSION['name'] . " (" . $_SESSION['role'] . ")</h1>";
+            // TODO: Replace echo with redirection code later
+        } else {
+            echo "<h1>Error: Incorect password.</h1> <a href='index.html'>Try again</a>";
+        }
+    } else {
+        echo "<h1>Error: No account found with that email.</h1> <a href='index.html'>Try again</a>";
+    }
+    $stmt->close();
+}
+$conn->close();
+?>
